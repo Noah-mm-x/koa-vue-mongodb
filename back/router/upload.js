579fc2d7eaa983a3fd25b02e10e59c2e2f2a0756
孟凡旭 <mengfanxu@qimai.cn>
2019-02-13 19:07:22 +0800

    新建文件夹

--- back/router/upload.js
+++ back/router/upload.js
@@ -19,12 +19,15 @@ const path = require("path");
 //     }
 // }));
 const router = new Router();
-router.post('/upload',async (ctx, next) => {
+router.post('/upload', async (ctx, next) => {
     // 上传单个文件
     const file = ctx.request.files.img; // 获取上传文件
     // 创建可读流
     const reader = fs.createReadStream(file.path);
     let filePath = path.join(__dirname, '../home/website/images') + `/${file.name}`;
+    if (!fs.existsSync(filePath)) { 
+        fs.mkdirSync(filePath); 
+    }
     // 创建可写流
     const upStream = fs.createWriteStream(filePath);
     // 可读流通过管道写入可写流
